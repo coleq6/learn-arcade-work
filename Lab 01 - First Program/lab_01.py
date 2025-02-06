025ad0d00e2d89c8b2551e6cfac3e6a303071c5d
coleq6 <coleq@hartwick.edu>
2025-02-06 08:54:20 -0500

    Another Test

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,4 @@
-its really cold outside
+print("Mr.Krabs:\"Hey Sponge Boy Me-Bob! You ready for some more overtime?\"")
+print("Spongebob:\"Oh boy you bet Mr.Krabs!\"")
+print("Squidward:\"But Mr.Krabs we've already been here for 24 straight hours!\"")
+print("Mr.Krabs:\"I don't care about that, I need to get me money!\"")
